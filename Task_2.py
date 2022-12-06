e807077c65e858130ff5c62c8c5a32355cddba4b
Pavel Anokhin <anohinpa@gmail.com>
2022-12-06 19:05:59 +0300

    Task_2_update

--- Task_2.py
+++ Task_2.py
@@ -1,5 +1,4 @@
 import random
-
 x = [random.randrange(-10, 10), random.randrange(-10, 10), random.randrange(-10, 10)]
 for i in range(10):
     print((not(x[0] or x[1] or x[2])) == (not(x[0]) and not(x[1]) and not(x[2])))
